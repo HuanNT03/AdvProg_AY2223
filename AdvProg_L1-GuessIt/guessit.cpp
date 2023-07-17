4faafade3877cfe958636f69e9d0c1843d4b130b
HuanNT03 <21021500@vnu.edu.vn>
2023-07-17 13:38:54 +0700

    Update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,9 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    
+    return (rand() % 100)+1;
+    //return 100;
 }
 
 
@@ -26,8 +28,11 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int guess;
+    cout << "What is your number: ";
+    cin >> guess;
+    return guess;
+    //return 1;
 }
 
 
@@ -46,6 +51,11 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
+    if (number > randomNumber){
+        answer = "Your number is higher.";
+    } else if (number < randomNumber){
+        answer = "Your number is lower.";
+    } else answer = "Congratulation! You win.";
 
     return answer;
 }
@@ -59,7 +69,8 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
+    if (answer == "Your number is higher." || answer == "Your number is lower.") return false;
+    else if (answer == "Congratulation! You win.")
     return true;
 }
 
@@ -73,7 +84,9 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if (isContinued == 'y') {
+        result = true;
+    }
     return result;
 }
 
@@ -87,7 +100,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cout << "Do you want to play again?(y/n): " << endl;
+    cin >> isContinued;
     return isContinued;
 }
 
